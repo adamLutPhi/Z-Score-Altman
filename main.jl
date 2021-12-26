6b03c627d2dbf3f2ddc39970536a6bd06d9ad915
ahmadlutfi <obzajd@gmail.com>
2021-12-26 18:58:34 -0500

    updates currentliabilities function

--- main.jl
+++ main.jl
@@ -1,14 +1,22 @@
 # TODO: Altman's z score
 
 
-function altmanCoeffs(currentAssets,currentLiabilities,totalAssets,)
+function altmanCoeffs(currentAssets,currentLiabilities,totalAssets,
+    retainedEarnings,earnings,capital,totalLiabilities,sales )
 
 #=
 CheatSheet:
 
 totalAssets goes into: X1, X2, X3
 =#
-workingCapital = currentAssets - currentLiabilities
+print("welcome to Altman z-score Julia Edition!\n")
+notespayable = input("please enter 'Notes Payable'")
+accountspayable = input("please enter 'Accounts Payable'")
+
+currentLiabs = currentliabilities(notespayable,accountspayable,)
+currentAssets = input("please enter 'Current Assets' ")
+workingCapital = currentAssets - currentLiabs
+print("Working Capital is: $ $workingCapital !")
 
 
 #= X1 = working Capital / total assets
@@ -27,14 +35,27 @@ X2 = retainedEarnings / totalAssets
 # X3 = earnings / total assets
 X3 = earnings / totalAssets
 
-# X4 = equity / total liabilities
+# X4 = equity(capital) / total liabilities
 
-X4 = equity / totalLiabilities
+X4 = capital / totalLiabilities
 
 # X5 = sales / total assets
 
 X5 = sales / totalAssets
 
-
 return X1,X2,X3,X4,X5
 end
+
+#notesPayable, accountsPayable, accruedExpense,unearnedRevenue, longtermDebt
+function currentliabilities()
+message = "please enter "
+    notesPayable =  input("$message 'Notes Payable'\n")
+    accountsPayable = input("$message 'accounts Payable'\n")
+    accruedExpense = input("$message 'Accrued Expense'\n")
+    unearnedRevenue = input("$message 'Unearned Revenue'\n")
+    longtermDebt = input("$message 'Long-term Debt'\n")
+    return  notesPayable + accountsPayable + accruedExpense + unearnedRevenue + longtermDebt
+end
+
+currentliabs = currentliabilities()
+altmanCoeffs()
