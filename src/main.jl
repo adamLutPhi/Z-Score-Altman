1921db0d2613119ef35db5a34d81cb0d426cc23d
ahmadlutfi <obzajd@gmail.com>
2021-12-28 18:23:20 -0500

    rewrites comment

--- src/main.jl
+++ src/main.jl
@@ -292,7 +292,7 @@ noncurrentliabs: LIABILITIES on the Long-term ; more than 1 Year
 
 currentliabs = currentliabilities() #done # omg my fave guitar : golden silver , soft & tender.. yes & no my true love s eye autumns comming  -- ive got to sing write my heart -focus
 noncurrentliabs =  tryparse(:Number ,input("please input Current Liabilities") )
-return # round  the burning circels , and then , the winter ....  # dislike please - golden sliver , all too soon - milk n honey, word & honeyyy# word * feels the cold weather]*
+return # continue singing: round & round...  the burning circels , and then , the winter ....  # dislike please - golden sliver , all too soon - milk n honey, word & honeyyy# word * feels the cold weather]*
 altmanCoeffs()
 
 
