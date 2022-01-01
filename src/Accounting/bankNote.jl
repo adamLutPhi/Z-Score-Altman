0c61882ab854fd82814a293da604ce7a82c87b89
ahmadlutfi <obzajd@gmail.com>
2022-01-01 01:43:32 -0500

    adds Accounting directory

---
+++ src/Accounting/bankNote.jl
@@ -0,0 +1,43 @@
+module bankNote
+
+#--- functions
+
+function _5dollar(value=5)
+end
+function _10dollar(value=10)
+end
+function _20dollar(value=20)
+
+end
+
+
+
+
+#TODO: time of start counting of Inflation -i.e. inflation, since When?
+"""
+a generic dollar amount
+has a value
+Changing based on inflation
+"""
+a generic dollar amount , has a value Changing based on inflation
+#
+function _Xdollar(value, ,inflation=10^-1)
+
+    intrinsic_value =  (value - value * inflation)
+    #if has
+end
+
+#The must-have factor
+"""
+based on netwon's renowned f = m * a in classical phyiscs
+a dollar has a physical value, that is changing by changin the
+1. velocity:
+2. acceleration:
+
+"""
+function _Xdollar(value, velocity, acceleration,inflation=10^-1)
+
+    intrinsic_value =  (value - value * inflation)
+    #if has
+end
+end
