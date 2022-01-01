58e994642cc59da0d74cd3a0148ec64c9d00575e
ahmadlutfi <obzajd@gmail.com>
2022-01-01 01:28:34 -0500

    adds TAccount

---
+++ src/TAccount.jl
@@ -0,0 +1,81 @@
+#= #an oop mentality was to tackle the object!
+function Account()
+
+end
+=#
+
+module Account
+
+Enum accountType = "Debit","Credit"
+
+end
+
+function increment(account, amount)
+account =  account + amount
+
+return account
+end
+
+function decrement(account, amount)
+account = account - amount
+end
+
+function DebitAccount(date,account , amount)
+
+end
+
+function CreditAccount(date, account ,amount)
+
+
+end
+
+
+
+
+
+end
+"""
+transaction Module
+
+each Transation Ideally must get written in 3 locations (at least ):
+forming the
+Accounting Triad
+1. A Journal (daily journal of transactions) -of the self (entity)
+2. A Debit Dr. Book
+3. A Credit Cr. Book
+
+Books can increase + or decrease in value -
+
+but each increment in a book has a
+
+"""
+module transaction
+Id:: Int # Auto-Increments
+
+function run()
+DebitAmount(date, amount )
+CreditAmount(date, amount)
+end
+
+function DebitAccount(date, amount)
+#TODO: add data row of CreditAccount here
+ #date Float64 amount
+end
+
+function CreditAccount(date,amount)
+#TODO: add data row of DebitAccount here
+# Date Float64 amount
+end
+
+function BalanceAccount()
+#TODO: check if today is last day in month
+bal_cd # @end of month Only - balance carried douwnd
+
+end
+function bal_cd()
+
+end
+
+end
+
+transaction()
