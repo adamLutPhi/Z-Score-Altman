043b94244c2b73356ed5bc873e4458b0615af503
ahmadlutfi <obzajd@gmail.com>
2022-01-14 12:56:55 -0500

    Commit:sanity checks extras.jl

--- src/Accounting/extras.jl
+++ src/Accounting/extras.jl
@@ -7,23 +7,3 @@ a necssacity for those needing mental reflush
 cleaner to return nothing in this case
 There are 3 (equally valid) ways I might use to do this. All 3 return nothing:
 """
-Method 1
-
-function myfunc()
-    #do something
-    return
-end
-Method 2
-
-function myfunc()
-    #do something
-    return nothing
-end
-Method 3
-
-function myfunc()
-    #do something
-    nothing
-end
-
-end  # module extras
