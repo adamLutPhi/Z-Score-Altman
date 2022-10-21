d882fe1c995e13a6db450e34f4593344caa13179
Ahmad Lutfi <obzajd@gmail.com>
2022-10-20 22:23:21 -0400

    Delete src/Accounting directory

--- src/Accounting/extras.jl
+++
@@ -1,9 +0,0 @@
-module extras
-
-
-"""
-these methods are optional (coming from a C variant language)
-a necssacity for those needing mental reflush
-cleaner to return nothing in this case
-There are 3 (equally valid) ways I might use to do this. All 3 return nothing:
-"""
